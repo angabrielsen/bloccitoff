1034486e86a66cb51a5512ac4e470dc687c434bc
Ashton Levier <anlevier@gmail.com>
2016-02-05 00:33:16 -0700

    eh i don't like where this is, but it technically works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'users/show'
 
-  root to: 'welcome#index'
+  root to: 'users#show'
 
   devise_for :users
+
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
