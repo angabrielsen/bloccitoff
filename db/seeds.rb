345bc7bf154643e99acd52e6d13db35a16816507
Ashton Levier <anlevier@gmail.com>
2016-02-13 16:12:33 -0700

    seeds finsihed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 require "faker"
 
-10.times do
-	user = User.create!(
-		name: Faker::Name.name,
-		email: Faker::Internet.email,
-		password: Faker::Internet.password(8)
-		)
-end
+ashton = User.create!(
+	name: 'backl_ash',
+	email: 'ashton@example.com',
+	password: 'helloworld'
+)
 
 10.times do
 	Item.create!(
-		user: users.sample,
+		user: User.first,
 		name: Faker::Lorem.sentence
 	)
 end
 
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Item.count} to-do items created"
