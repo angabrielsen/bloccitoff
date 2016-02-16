5dc92e7b9460c1e692db268898458346ec43f7bb
Ashton Levier <anlevier@gmail.com>
2016-02-15 23:12:23 -0700

    moved to helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,7 @@
 module ItemsHelper
+	include ActionView::Helpers::DateHelper
+
+  def time_left(item)
+  	distance_of_time_in_words(Time.now, item.due_date)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
-	include ActionView::Helpers::DateHelper
 
   belongs_to :user
 
@@ -7,9 +6,5 @@ class Item < ActiveRecord::Base
 
   def due_date
   	self.created_at + 7.days
-  end
-
-  def time_left
-  	distance_of_time_in_words(Time.now, due_date)
   end
 end
