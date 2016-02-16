995a3dbb39d1bf0175ba3dd7d6b49369421cc61e
Ashton Levier <anlevier@gmail.com>
2016-02-15 22:30:32 -0700

    time left

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ActiveRecord::Base
+	include ActionView::Helpers::DateHelper
+
   belongs_to :user
 
   validates :name, length: { minimum: 5 }, presence: true
+
+  def due_date
+  	self.created_at + 7.days
+  end
+
+  def time_left
+  	distance_of_time_in_words(Time.now, due_date)
+  end
 end
